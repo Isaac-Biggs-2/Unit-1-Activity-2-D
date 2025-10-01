a4df4b8747507762317582c8b8c1d8d90e68789c
default <isaac.biggs@stu.ocsb.ca>
2025-10-01 13:25:45 +0000

    added program to display name and courses to terminal

---
+++ program-1/main.ts
@@ -0,0 +1,24 @@
+/**
+ * @author Isaac Biggs
+ * @version 1.0.0
+ * @date @2025-10-01
+ * @fileoverview A program to display my name as well as my courses.
+ */
+
+// displays my name
+console.log("Isaac Biggs");
+
+//displays course
+console.log("ICS3UV Computer Science");
+
+//displays course
+console.log("SCH3UR Chemistry");
+
+//displays course
+console.log("NBE3U English");
+
+//displays course
+console.log("HRT3MR World Religions")
+
+console.log("\nDone.")
+
