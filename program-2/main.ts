12b98069a633fbc9b0524fa62cfcf67348cce8f0
default <isaac.biggs@stu.ocsb.ca>
2025-10-03 13:34:17 +0000

    Added program to display hail Mary prayer

---
+++ program-2/main.ts
@@ -0,0 +1,32 @@
+/**
+ * @author Isaac Biggs
+ * @version 1.0.0
+ * @date @2025-10-03
+ * @fileoverview A program to display the hail Mary prayer.
+ */
+
+//displays line of prayer
+console.log("Hail, Mary,fullof grace,")
+
+//displays line of prayer
+console.log("the Lord is with thee.")
+
+//displays line of prayer
+console.log("Blessed art thou amongst women")
+
+//displays line of prayer
+console.log("and blessed is the fruit of thy womb, Jesus.")
+
+//displays line of prayer
+console.log("Holy Mary, Mother of God,")
+
+//displays line of prayer
+console.log("pray for us sinners,")
+
+//displays line of prayer
+console.log("now and at the hour of our death.")
+
+//displays line of prayer
+console.log("Amen.")
+
+console.log("\nDone.")
