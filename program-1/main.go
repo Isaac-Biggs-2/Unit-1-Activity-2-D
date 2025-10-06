3b8e29d5372ec1fd79149feddb033ed011bdd56e
default <isaac.biggs@stu.ocsb.ca>
2025-10-06 12:38:54 +0000

    Added program to display name and courses in GO

---
+++ program-1/main.go
@@ -0,0 +1,30 @@
+/*
+ *@author Isaac Biggs
+ *@version 1.0.0
+ *date 2025-10-0106
+ *fileoverview This program displays my name and coursed
+ */
+
+ package main
+
+ import "fmt"
+
+ func main() {
+
+	//Prints my name
+	fmt.Println("Isaac Biggs")
+
+	//Prints course with code
+	fmt.Println("ICS3UV Computer Science")
+
+	//Prints course with code
+	fmt.Println("SCH3UR Chemistry")
+
+	//Prints course with code
+	fmt.Println("NBE3U English")
+
+	//Prints course with code
+	fmt.Println("HRT3MR World Religions")
+
+	fmt.Println("\nDone.")
+ }
