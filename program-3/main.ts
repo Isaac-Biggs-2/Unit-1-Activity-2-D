64c4ddfef420e9c2d871ff0d62f923798472776a
default <isaac.biggs@stu.ocsb.ca>
2025-10-06 12:29:40 +0000

    added program that displays happy birthday song

---
+++ program-3/main.ts
@@ -0,0 +1,20 @@
+/**
+ * @author Isaac Biggs
+ * @version 1.0.0
+ * @date @2025-10-06
+ * @fileoverview A program to display the happy birthday song.
+ */
+
+//displays line of the song
+console.log("Happy birthday to you");
+
+//displays line of the song
+console.log("Happy birthday to you");
+
+//displays line of the song
+console.log("Happy birthday, dear Isaac");
+
+//displays  line of the song
+console.log("Happy birthday to you.");
+
+console.log("\nDone.")
